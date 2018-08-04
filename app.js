59d81a379742bbd761362e141d312aac651bd542
Zayne <32248549+Zhenye-Na@users.noreply.github.com>
2018-08-04 17:16:13 -0500

    update

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
+// server.js
+// load the things we need
 var express = require("express");
 var app = express();
 
+// set the view engine to ejs
+app.set('view engine', 'ejs');
 
+// index page 
 app.get("/", function(req, res) {
-    // body...
+    res.render("landing");
 })
+
+// Camp grounds page
+app.get("/campgrounds",  function(req, res) {
+    var campgrounds = [
+        {name: "hha", image: "123123"},
+        {name: "hha", image: "123123"},
+        {name: "hha", image: "123123"}
+        ];
+    
+    res.render("campgrounds", {campgrounds: campgrounds});
+})
+
+
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log("hahaha");
+});
